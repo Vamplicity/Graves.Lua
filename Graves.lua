5023d86a9764759388521fffee41a9b6ee3d8465
Vamplicity <106854604+Vamplicity@users.noreply.github.com>
2022-07-13 23:30:46 -0500

    Update Graves.lua

--- Graves.lua
+++ Graves.lua
@@ -51,29 +51,29 @@ local Label1 = Section1:CreateLabel("Label 1")
 Label1:UpdateText("Dahood")
 -------------
 local Button1 = Section1:CreateButton("Inject", function()
-    print("Click Button 1")
+
 end)
 Button1:AddToolTip("Press One Time It Will Notify When Loaded")
 -------------
 local Toggle1 = Section1:CreateToggle("Toggle 1", nil, function(State)
-    print(State)
+
 end)
 Toggle1:AddToolTip("Toggle 1 ToolTip")
 Toggle1:CreateKeybind("Y", function(Key)
-    print(Key)
+
 end)
 
 local TextBox1 = Section1:CreateTextBox("TextBox 1", "Only numbers", true, function(Value)
-    print(Value)
+
 end) 
 TextBox1:AddToolTip("Yes only numbers")
 TextBox1:SetValue("new value here")
 Section1:CreateTextBox("TextBox 1\nMultiline", "numbers and letters", false, function(String)
-    print(String)
+
 end)
 -------------
 local Slider1 = Section1:CreateSlider("Slider 1", 70,120,nil,true, function(Value)
-    print(Value)
+
 end)
 Slider1:AddToolTip("This Changes Fov Of Your Camera")
 Slider1:SetValue(70)
@@ -84,7 +84,7 @@ Dropdown1:AddToolTip("Dropdown 1 ToolTip")
 Dropdown1:SetOption("Lock") 
 -------------
 local Colorpicker1 = Section1:CreateColorpicker("Colorpicker 1", function(Color)
-    print(Color)
+
 end)
 Colorpicker1:AddToolTip("Colorpicker 1 ToolTip")
 Colorpicker1:UpdateColor(Color3.fromRGB(255,0,0))
@@ -92,32 +92,32 @@ Colorpicker1:UpdateColor(Color3.fromRGB(255,0,0))
 Section2:CreateLabel("Label 2\nMultiline")
 -------------
 local Button2 = Section2:CreateButton("Button 2\nMultiline", function()
-    print("Click Button 2")
+
 end)
 Button2:AddToolTip("Button 2 ToolTip\nMultiline")
 -------------
 local Toggle2 = Section2:CreateToggle("Toggle 2\nMultiline", nil, function(State)
-    print(State)
+
 end)
 Toggle2:AddToolTip("Toggle 2 ToolTip\nMultiline")
 Toggle2:CreateKeybind("U", function(Key)
-    print(Key)
+
 end)
 -------------
 local Slider2 = Section2:CreateSlider("Slider 2\nMultiline", 0,100,nil,false, function(Value)
-    print(Value)
+
 end)
 Slider2:AddToolTip("Slider 2 ToolTip\nMultiline")
 Slider2:SetValue(25)
 -------------
 local Dropdown2 = Section2:CreateDropdown("Dropdown 2\nMultiline", {"Option 4","Option 5","Option 6"}, function(String)
-    print(String)
+
 end)
 Dropdown2:AddToolTip("Dropdown 2 ToolTip")
 Dropdown2:SetOption("Option 6")
 -------------
 local Colorpicker2 = Section2:CreateColorpicker("Colorpicker 2\nMultiline", function(Color)
-    print(Color)
+
 end)
 Colorpicker2:AddToolTip("Colorpicker 2 ToolTip")
 Colorpicker2:UpdateColor(Color3.fromRGB(0,0,255))
